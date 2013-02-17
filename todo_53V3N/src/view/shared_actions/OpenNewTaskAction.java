b0439cef3c45a84ecc5db294fbbf12846d6fb4c7
Madde <Madde@212.25.149.117>
2013-02-17 14:10:56 +0100

    Add menu and empty (shared) actions classes.

---
+++ todo_53V3N/src/view/shared_actions/OpenNewTaskAction.java
@@ -0,0 +1,16 @@
+package view.shared_actions;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import view.new_task_dialog.NewTaskDialog;
+
+public class OpenNewTaskAction extends AbstractAction {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
+	}
+
+}
---
+++ todo_53V3N/src/view/shared_actions/SortByAction.java
@@ -0,0 +1,15 @@
+package view.shared_actions;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+public class SortByAction extends AbstractAction {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
